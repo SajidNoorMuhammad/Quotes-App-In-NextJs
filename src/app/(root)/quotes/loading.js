a9aacb5b2b195e037787c1db6e9de54ec2e81153
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-10 14:28:13 +0500

    done.

---
+++ src/app/(root)/quotes/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <div className=" text-[40px] text-green-600 flex justify-center my-auto">
+            Loading...
+        </div>
+    )
+}
+
+export default loading
