ca8aa9c1b40b077f134045d9fa19091f0c658da0
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-10 14:28:26 +0500

    final work.

--- src/app/(root)/quotes/page.jsx
+++ src/app/(root)/quotes/page.jsx
@@ -6,7 +6,9 @@ export const metadata = {
 
 const Quotes = async () => {
 
-    let quotes = await fetch('https://dummyjson.com/quotes');
+    let quotes = await fetch('https://dummyjson.com/quotes', {
+        cache : 'no-cache'
+    });
     quotes = await quotes.json();
     console.log(quotes)
 
@@ -16,7 +18,7 @@ const Quotes = async () => {
             <div className=" grid grid-cols-3 mx-auto container gap-3 max-sm:grid-cols-1 max-md:grid-cols-2 ">
                 {
                     quotes.quotes.map((quote) => (
-                        <div className=" border-2 border-green-700 rounded p-3">
+                        <div key={quote.id} className=" border-2 border-green-700 rounded p-3">
                             <h1 className=" font-serif space-y-2"><span className=" font-semibold font-serif text-[20px] text-green-600">Title:</span> <br />{quote.quote}</h1>
                             <h1 className=" mt-4 font-serif"><span className=" font-semibold">Author: </span> {quote.author}</h1>
                         </div>
